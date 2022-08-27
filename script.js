79e66c5e5c817e6449545b95588b5322bf090f04
kckuei <kevinkuei@gmail.com>
2022-08-27 14:07:13 -0700

    Refactor playRound function

--- script.js
+++ script.js
@@ -1,72 +1,75 @@
-function playRound(playerSelection, computerSelection) {
-
-    //            rock paper scissor
-    // rock       tie  lose  win
-    // paper      win  tie   lose
-    // scissor    lose  win  tie
-
-    // A more elegant solution is to encode the decision matrix as a 2d array, 
-    // e.g. with numbers 0, 1, 2 being associated with tie, lose, win
-    // Create helper functions to map rock, paper, scissor to 1, 2, or 3
-    // Then to look up the win/loss/win condition appropriately. 
-    // Then we would only need one block of code that returns tie, lose or win.
-
-    playerSelection = processInput(playerSelection);
-    computerSelection = processInput(computerSelection);
-
-    if (playerSelection === "Rock") {
-        if (computerSelection === "Rock") {
-            return 'Tie!'
-        } else if (computerSelection === "Paper") {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
-        } else {
-            return `You Win!  ${playerSelection} beats ${computerSelection}`
-        }
-    } else if (playerSelection === "Paper") {
-        if (computerSelection === "Rock") {
-            return `You Win!  ${playerSelection} beats ${computerSelection}`
-        } else if (computerSelection === "Paper") {
-            return 'Tie!'
-        } else {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
-        }
-    } else {
-        if (computerSelection === "Rock") {
-            return `You Lose! ${computerSelection} beats ${playerSelection}`
-        } else if (computerSelection === "Paper") {
-            return `You Win!  ${playerSelection} beats ${computerSelection}`
-        } else {
-            return 'Tie!'
-        }
+function getUserChoice() {
+    let userSelection = "";
+    while (!["Rock", "Paper", "Scissor"].includes(userSelection)) {
+        userSelection = prompt("Enter rock, paper, or scissor.", getComputerChoice());
     }
+    return userSelection;
 }
+
 
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     randomInt = getRandomRangeInt(1, choices.length);
-    return choices[randomInt - 1]
+    return choices[randomInt - 1];
 }
+
 
 function getRandomRangeInt(min, max) {
-    return Math.round(Math.random() * (max - min) + min)
+    return Math.round(Math.random() * (max - min) + min);
 }
 
-function processInput(string) {
+
+function parseInput(string) {
     string = string.toLowerCase();
-    return string[0].toUpperCase() + string.slice(1)
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+
+function playRound(playerSelection, computerSelection) {
+
+    playerSelection = parseInput(playerSelection);
+    computerSelection = parseInput(computerSelection);
+
+    // decision matrix
+    //            rock paper scissors
+    // rock       tie  lose  win
+    // paper      win  tie   lose
+    // scissors   lose  win  tie
+    let matrix = {
+        'Rock': {
+            'Rock': 'tie',
+            'Paper': 'lose',
+            'Scissor': 'win'
+        },
+        'Paper': {
+            'Rock': 'win',
+            'Paper': 'tie',
+            'Scissor': 'lose'
+        },
+        'Scissors': {
+            'Rock': 'lose',
+            'Paper': 'win',
+            'Scissor': 'tie'
+        },
+    };
+
+    if (matrix[playerSelection][computerSelection] === 'win') {
+        return `You Win!  ${playerSelection} beats ${computerSelection}`;
+    } else if (matrix[playerSelection][computerSelection] === 'lose') {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    } else {
+        return 'Tie!';
+    }
 }
 
 function game(numRounds) {
 
-    userWins = 0;
-    computerWins = 0;
-    ties = 0;
+    let userWins = 0;
+    let computerWins = 0;
+    let ties = 0;
 
     for (let i = 0; i < numRounds; i++) {
-        let userSelection = "";
-        while (!["Rock", "Paper", "Scissors"].includes(userSelection)) {
-            userSelection = prompt("Enter rock, paper, or scissors.", getComputerChoice());
-        }
+        let userSelection = getUserChoice()
         let computerSelection = getComputerChoice();
         let result = playRound(userSelection, computerSelection);
 
@@ -81,17 +84,19 @@ function game(numRounds) {
             ties += 1;
         }
     }
+
+    reportWinner(userWins, computerWins, ties, numRounds);
 }
 
-function reportWinner(userWins, computerWins, ties) {
+function reportWinner(userWins, computerWins, ties, numRounds) {
     if (userWins > computerWins) {
-        console.log(`User Wins Game! User Won ${userWins} Out of ${numRounds} Rounds! Number of Ties = ${ties}`)
+        console.log(`User Wins Game! User Won ${userWins} Out of ${numRounds} Rounds! Number of Ties = ${ties}`);
     } else if (computerWins > userWins) {
-        console.log(`User Lost Game! User Won ${userWins} Out of ${numRounds} Rounds! Number of Ties = ${ties}`)
+        console.log(`User Lost Game! User Won ${userWins} Out of ${numRounds} Rounds! Number of Ties = ${ties}`);
     } else {
-        console.log('It\'s a Tie!')
+        console.log('It\'s a Tie!');
     }
 }
 
 
-game(5)
+game(5);
